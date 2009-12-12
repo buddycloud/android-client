ec9f13862f2dc85306bcfa407c3005b7298aa290
rtreffer <rtreffer@3fc15f3e-b8c3-11de-ba48-051263943efc>
2009-12-12 14:42:16 +0000

    Move connect / login / updateRoaster into seperate methods, and bind to onStart
    git-svn-id: http://buddycloud.googlecode.com/svn/trunk/client/android/main@130 3fc15f3e-b8c3-11de-ba48-051263943efc

--- src/com/buddycloud/android/buddydroid/BuddycloudService.java
+++ src/com/buddycloud/android/buddydroid/BuddycloudService.java
@@ -23,98 +23,137 @@ import com.buddycloud.jbuddycloud.provider.PubSubLocationEventProvider;
 
 public class BuddycloudService extends Service {
 
-        
-        private static final String TAG = "Service";
-        private BuddycloudClient mConnection;
+    private static final String TAG = "Service";
+    private BuddycloudClient mConnection;
 
-        @Override
-        public void onCreate() {
-                Log.d(TAG, " onCreate");
+    @Override
+    public void onCreate() {
+        Log.d(TAG, " onCreate");
+        super.onCreate();
+    }
 
-                SharedPreferences pm = PreferenceManager.getDefaultSharedPreferences(this);
-                String password = pm.getString("password", "");
-                String jid = pm.getString("jid", "initial@place.holder");
-
-                // Create a connection
-//              connConfig.setReconnectionAllowed(true);
-                mConnection = new BuddycloudClient(jid.split("@")[1]);
-
-                // Step 1, try to connect
-                try {
-                	//                      SmackConfiguration.setPacketReplyTimeout(12000);
-                	mConnection.connect();
-                	Log.i(TAG, "connected to " + mConnection.getHost() + ":" + mConnection.getPort());
-                } catch (Exception ex) {
-                	Log.e("XMPPClient", "Failed to connect to " + mConnection.getHost());
-                	Log.e("XMPPClient", ex.toString());
-                }
-
-                // Step 2, try to Authanticate
-                try {
-                	mConnection.login(jid, password);
-                } catch (Exception ex) {
-                    try {
-                    	String usr = jid.substring(0, jid.lastIndexOf('@'));
-                    	mConnection.login(usr, password);
-                    } catch (Exception ex2) {
-                    	Log.e("XMPPClient", "Login as " + jid + " failed");
-                    	Log.e("XMPPClient", ex.toString());
-                    	Log.e("XMPPClient", "Login as " + jid.split("@")[0] + " failed");
-                    	Log.e("XMPPClient", ex2.toString());
-                    }
-                }
-
-                if (mConnection.isAuthenticated()) {
-                	Toast.makeText(this, "Login successful :)", Toast.LENGTH_SHORT).show();
-                	
-                	// retrieve roster
-                	getContentResolver().delete(Roster.CONTENT_URI, null, null);
-                	for (Iterator iterator = mConnection.getRoster().getEntries().iterator(); iterator.hasNext();) {
-                		RosterEntry buddy = ((RosterEntry) iterator.next());
-//                		Log.d(TAG, buddy.getUser()+" "+buddy.getName()+" "+buddy.getStatus());
-                		ContentValues values = new ContentValues();
-                		values.put(Roster.JID, buddy.getUser());
-                		values.put(Roster.NAME, buddy.getUser().split("\\@")[0]);
-                		getContentResolver().insert(Roster.CONTENT_URI, values);
-                	}
-                	getContentResolver().notifyChange(Roster.CONTENT_URI, null);
-
-                	mConnection.addPacketListener(new PacketListener() {
-
-                		@Override
-                		public void processPacket(Packet packet) {
-                			//                      Log.d(TAG, "Packet: "+packet.toXML());
-                			if (packet instanceof Message) {
-                				LocationEvent loc = (LocationEvent) packet.getExtension(PubSubLocationEventProvider.getNS());
-                				if (loc != null) {
-                					Log.d(TAG, "GEOLOC received: "+packet.getFrom()+" -> "+loc.text);
-                					ContentValues values = new ContentValues();
-                					switch (loc.type) {
-                					case LocationEvent.CURRENT:
-                                		values.put(Roster.GEOLOC, loc.text);
-                						break;
-                					case LocationEvent.PREV:
-                						values.put(Roster.GEOLOC_PREV, loc.text);
-                						break;
-                					case LocationEvent.NEXT:
-                						values.put(Roster.GEOLOC_NEXT, loc.text);
-                						break;
-                					}
-                					getContentResolver().update(Roster.CONTENT_URI, values, Roster.JID+"='"+packet.getFrom()+"'", null);
-                				}
-                			}
-                		}}, null);
-                } else {
-                	Toast.makeText(this, "Login failed :(", Toast.LENGTH_LONG).show();
-                }
-                super.onCreate();
+    public void createConnection() {
+        if (mConnection != null && mConnection.isConnected()) {
+           return;
         }
+
+        SharedPreferences pm =
+            PreferenceManager.getDefaultSharedPreferences(this);
+
+        String jid = pm.getString("jid", "");
+        if (jid.indexOf('@') == -1) {
+            return;
+        }
+
+        String domain = jid.substring(jid.lastIndexOf('@') + 1);
+        mConnection = new BuddycloudClient(domain);
+        try {
+            mConnection.connect();
+            Log.i(TAG, "connected to " + mConnection.getHost() + ":" + mConnection.getPort());
+        } catch (Exception ex) {
+            Log.e("XMPPClient", "Failed to connect to " + mConnection.getHost());
+            Log.e("XMPPClient", ex.toString());
+        }
+
+        if (mConnection.isConnected()) {
+            mConnection.addPacketListener(new PacketListener() {
+
+                @Override
+                public void processPacket(Packet packet) {
+                    if (packet instanceof Message) {
+                        LocationEvent loc = (LocationEvent) packet.getExtension(PubSubLocationEventProvider.getNS());
+                        if (loc != null) {
+                            Log.d(TAG, "GEOLOC received: "+packet.getFrom()+" -> "+loc.text);
+                            ContentValues values = new ContentValues();
+                            switch (loc.type) {
+                            case LocationEvent.CURRENT:
+                                values.put(Roster.GEOLOC, loc.text);
+                                break;
+                            case LocationEvent.PREV:
+                                values.put(Roster.GEOLOC_PREV, loc.text);
+                                break;
+                            case LocationEvent.NEXT:
+                                values.put(Roster.GEOLOC_NEXT, loc.text);
+                                break;
+                            }
+                            getContentResolver().update(Roster.CONTENT_URI, values, Roster.JID+"='"+packet.getFrom()+"'", null);
+                        }
+                    }
+                }}, null);
+        }
+    }
+
+    public void login() {
+        if (mConnection == null || !mConnection.isConnected()) {
+            return;
+        }
+
+        SharedPreferences pm =
+            PreferenceManager.getDefaultSharedPreferences(this);
+
+        String jid = pm.getString("jid", "");
+        if (jid.indexOf('@') == -1) {
+            return;
+        }
+        String password = pm.getString("password", "");
+        jid = jid + "@";
+
+        // We try to login with every '@'-seperated prefix
+        // e.g. user@gmail.com@googlemail.com
+        // will be tried as
+        // user@gmail.com@googlemail.com
+        // user@gmail.com
+        // user
+        do {
+            jid = jid.substring(0, jid.lastIndexOf('@'));
+            try {
+                mConnection.login(jid, password);
+            } catch (Exception ex) {
+                Log.e("XMPPClient", "Login as " + jid + " failed");
+                Log.e("XMPPClient", ex.toString());
+            }
+        } while (!mConnection.isAuthenticated() && jid.indexOf('@') != -1);
+
+        if (mConnection.isAuthenticated()) {
+            Toast.makeText(this, "You are online!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Login failed :(", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void updateRoaster() {
+        if (mConnection == null ||
+           !mConnection.isConnected() ||
+           !mConnection.isAuthenticated()
+        ) {
+            return;
+        }
+
+        getContentResolver().delete(Roster.CONTENT_URI, null, null);
+        Iterator iterator = mConnection.getRoster().getEntries().iterator();
+        while (iterator.hasNext()) {
+            RosterEntry buddy = ((RosterEntry) iterator.next());
+            ContentValues values = new ContentValues();
+            values.put(Roster.JID, buddy.getUser());
+            values.put(Roster.NAME, buddy.getUser().split("\\@")[0]);
+            getContentResolver().insert(Roster.CONTENT_URI, values);
+        }
+        getContentResolver().notifyChange(Roster.CONTENT_URI, null);
+    }
 
         @Override
         public void onStart(Intent intent, int startId) {
-                Log.d(TAG, " onStart");
-                
-                super.onStart(intent, startId);
+            Log.d(TAG, " onStart");
+            super.onStart(intent, startId);
+
+            if (mConnection == null ||
+                !mConnection.isConnected() ||
+                !mConnection.isAuthenticated()
+            ) {
+                createConnection();
+                login();
+                updateRoaster();
+            }
         }
 
         @Override
