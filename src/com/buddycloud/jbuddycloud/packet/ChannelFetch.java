3e8f0e7e9864efc03014a24fe686eb70ebf23f23
rtreffer <rtreffer@3fc15f3e-b8c3-11de-ba48-051263943efc>
2009-12-25 14:49:47 +0000

    Packet to trigger a channel catchup
    git-svn-id: http://buddycloud.googlecode.com/svn/trunk/client/android/main@190 3fc15f3e-b8c3-11de-ba48-051263943efc

---
+++ src/com/buddycloud/jbuddycloud/packet/ChannelFetch.java
@@ -0,0 +1,28 @@
+package com.buddycloud.jbuddycloud.packet;
+
+import org.jivesoftware.smack.packet.IQ;
+
+public class ChannelFetch extends IQ {
+
+    private final long since;
+    private final String node;
+
+    public ChannelFetch(String node, long since) {
+        this.node = node;
+        this.since = since;
+        setTo("broadcaster.buddycloud.com");
+        setType(Type.GET);
+    }
+
+    @Override
+    public String getChildElementXML() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<pubsub xmlns='http://jabber.org/protocol/pubsub'><items node='")
+          .append(node)
+          .append("'><set xmlns='http://jabber.org/protocol/rsm'><after>")
+          .append(Long.toString(since))
+          .append("</after></set></items></pubsub>");
+        return sb.toString();
+    }
+
+}
