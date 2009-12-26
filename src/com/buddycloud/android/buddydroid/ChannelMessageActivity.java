969dcb7ab8a309039bbb2e86e94c459b31a34e4b
rtreffer <rtreffer@3fc15f3e-b8c3-11de-ba48-051263943efc>
2009-12-26 11:47:34 +0000

    channel-indicator for message authors aff (moderator, owner, ...)
    git-svn-id: http://buddycloud.googlecode.com/svn/trunk/client/android/main@202 3fc15f3e-b8c3-11de-ba48-051263943efc

--- src/com/buddycloud/android/buddydroid/ChannelMessageActivity.java
+++ src/com/buddycloud/android/buddydroid/ChannelMessageActivity.java
@@ -3,9 +3,11 @@ package com.buddycloud.android.buddydroid;
 import android.app.ListActivity;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
@@ -13,8 +15,18 @@ import com.buddycloud.android.buddydroid.provider.BuddyCloud.ChannelData;
 
 public class ChannelMessageActivity extends ListActivity {
 
+    private int transparent;
+    private int owner;
+    private int moderator;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        transparent = Color.argb(0, 0, 0, 0);
+        owner = Color.argb(255, 241, 27, 27);
+        moderator = Color.argb(255, 246, 169, 43);
 
         String node = getIntent().getData().toString().substring(8);
 
@@ -43,6 +55,34 @@ public class ChannelMessageActivity extends ListActivity {
             tv.setText(cursor.getString(
                 cursor.getColumnIndex(ChannelData.CONTENT)
             ));
+            long p = cursor.getLong(cursor.getColumnIndex(ChannelData.PARENT));
+            if (p == 0l) {
+                tv.setPadding(
+                    30,
+                    tv.getPaddingTop(),
+                    tv.getPaddingRight(),
+                    tv.getPaddingBottom()
+                );
+            } else {
+                tv.setPadding(
+                    5,
+                    tv.getPaddingTop(),
+                    tv.getPaddingRight(),
+                    tv.getPaddingBottom()
+                );
+            }
+            tv = (TextView) view.findViewById(R.id.tag);
+            String affiliation =
+                cursor.getString(cursor.getColumnIndex(
+                        ChannelData.AUTHOR_AFFILIATION));
+            if (affiliation.equals("owner")) {
+                tv.setBackgroundColor(owner);
+            } else
+            if (affiliation.equals("moderator")) {
+                tv.setBackgroundColor(moderator);
+            } else {
+                tv.setBackgroundColor(transparent);
+            }
         }
 
         @Override
